aa68ee373f78232b6f833473b1fc36961a7f412e
Alejandro Schwartz <alejandro.schwartz@gmail.com>
2013-07-12 11:50:53 +0000

    Test #10

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 app.get('/', function(request, response) {
 	
 	var buf = fs.readFileSync('index.html');
-    	buf.toString(buf);
+    	buf.toString(buf,'utf8');
 });
 
 var port = process.env.PORT || 5000;
