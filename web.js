ec23be2583ba6facfc7160b20b35054f9d9f0838
Alejandro Schwartz <alejandro.schwartz@gmail.com>
2013-07-12 11:03:26 +0000

    Test #3

--- web.js
+++ web.js
@@ -1,16 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var Iconv = require('iconv').Iconv;
 var fs = require('fs');
 
 app.get('/', function(request, response) {
 	
 	var content = fs.readFileSync(index.html);
-	var iconv = new Iconv(encoding, 'UTF-8');
-    	var buffer = iconv.convert(content);
-	
-  return buffer.toString('utf8');
+    	
+	buffer.toString(content, 'utf8');
 });
 
 var port = process.env.PORT || 5000;
